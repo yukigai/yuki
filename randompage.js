8f97d5568866b6f366a10d7a7a379921a280dcf5
yukigai <95469007+yukigai@users.noreply.github.com>
2021-12-03 17:20:56 +0900

    Update randompage.js

--- randompage.js
+++ randompage.js
@@ -1,5 +1,5 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-var links = ['https://outlook.office.com/mail/inbox', 'https://www.youtube.com/', 'https://twitter.com/','https://findy-code.io/?email_sent=true']; //同じようにして遷移先のURLを配列へ格納する
+var links = ['https://outlook.office.com/mail/inbox', 'https://www.youtube.com/', 'https://forms.gle/oe4pyo7BQRSeuSYp8','https://findy-code.io/?email_sent=true']; //同じようにして遷移先のURLを配列へ格納する
 window.location.href = links[getRandomInt(4)]; //getrandomInt(x)のxを遷移先のページの数に
